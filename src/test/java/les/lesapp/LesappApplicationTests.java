2a2bf3418f7166705291dda94116f0e60b5b1271
PedroHenriqueFonsecaMelo <83472390+PedroHenriqueFonsecaMelo@users.noreply.github.com>
2023-02-15 16:14:58 -0300

    Initial commit

---
+++ src/test/java/les/lesapp/LesappApplicationTests.java
@@ -0,0 +1,13 @@
+package les.lesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
